e2f3ff1fc0fbf602ed93baa2285d2c31029bb7e8
musta10 <mustafaenfadal@gmail.com>
2021-03-27 20:52:43 +0100

    listas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import Header from "./components/Header"
+import {BrowserRouter, Switch} from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      <h1>test</h1>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Header />
+      </Switch>
+      </BrowserRouter>
   );
 }
 
--- src/components/Topmusic.jsx
+++ src/components/Topmusic.jsx
@@ -9,20 +9,21 @@ class Topmusic extends Component {
         <img src={music} alt="music"/></div>
         
         <article className="video-top">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/37tmRDUK0FM" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        <iframe title="1" width="560" height="315" src="https://www.youtube.com/embed/37tmRDUK0FM" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
         </article>
         <article className="video-top">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/0Q5J38bpunI" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+        <iframe title="2"  width="560" height="315" src="https://www.youtube.com/embed/y4mlhGVN-Ek" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
         </article>
         <article className="video-top">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/j3Lp4M9K-uw" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+        <iframe title="3" width="560" height="315" src="https://www.youtube.com/embed/0Q5J38bpunI" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
         </article>
         <article className="video-top">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/XbM5uUdXe4k" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+        <iframe title="4" width="560" height="315" src="https://www.youtube.com/embed/j3Lp4M9K-uw" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
         </article>
         <article className="video-top">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/y4mlhGVN-Ek" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+        <iframe title="5" width="560" height="315" src="https://www.youtube.com/embed/XbM5uUdXe4k" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
         </article>
+ 
     </>
     );
   }
